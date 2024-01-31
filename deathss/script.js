3c3d5f0b30afaeed4de31143e32195afee2f9a44
zpel <157932765+zpel@users.noreply.github.com>
2024-01-31 20:01:57 -0300

    Add files via upload

---
+++ deathss/script.js
@@ -0,0 +1,60 @@
+const numeroCapitulos = 32
+let taTocando = 0;
+let capituloAtual = 1;
+
+const botaoPlayPause = document.getElementById("play-pause");
+const audioCapitulo = document.getElementById("audio-capitulo");
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const nomeCapitulo = document.getElementById('capitulo');
+
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill');
+}
+function pausarFaixa(){
+    audioCapitulo.pause();
+    botaoPlayPause.classList.add('bi-play-circle-fill');
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+}
+
+function tocarOuPausar(){
+    if (taTocando === 0){
+        tocarFaixa();
+        taTocando = 1;
+    } else {
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+function trocarNomeFaixa(){
+    nomeCapitulo.innerText = 'Capitulo ' + capituloAtual;
+}
+function proximaFaixa(){
+    if (capituloAtual === numeroCapitulos){
+        capituloAtual = 1;
+    }else{
+        capituloAtual = capituloAtual + 1
+
+    }
+    audioCapitulo.src = './deaths/' + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+function voltarFaixa(){
+    if (capituloAtual === 1){
+        capituloAtual = numeroCapitulos;
+    }else{
+        capituloAtual = capituloAtual - 1
+
+    }
+    audioCapitulo.src = './deaths/' + capituloAtual + '.mp3';
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa()
+}
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
